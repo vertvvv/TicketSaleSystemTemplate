aaac9df42110ce7190e3dcb29d770327b9ff0db6
vertvvv <vertvvv@gmail.com>
2016-11-23 19:57:59 +0300

    Attractions modal window add
    Attraction modal add, font fix in login modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$('#attractionModal').on('show.bs.modal', function (event) {
+    let button = $(event.relatedTarget);// Button that triggered the modal
+    let attractionName = button.data('name'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    let modal = $(this)
+    modal.find('.modal-title').text(attractionName);
+});
+
 function openModal(flag) {
     if (flag) {
         $("#tab-1").prop('checked', true);
@@ -5,3 +14,4 @@ function openModal(flag) {
         $("#tab-2").prop('checked', true);
     }
 }
+
