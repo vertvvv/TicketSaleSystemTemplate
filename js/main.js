f1b27b7c81378249b49a5c932de6840c61c5c6bb
vertvvv <vertvvv@gmail.com>
2016-11-23 05:32:36 +0300

    Login fix & code refactor
    Color & paddings (for mobile version) fix, some code refactor

--- js/main.js
+++ js/main.js
@@ -1,30 +1,7 @@
-var flag;
-flag = true; //flag for correct closing attraction boxes
-
-function box(element, flag) {
-    if(element && flag) {
-        if(element.className=='box') {
-            while(document.getElementById('main').getElementsByClassName('box target')[0]) {
-                document.getElementById('main').getElementsByClassName('box target')[0].className='box';
-            }
-            element.className='box target';
-        }
-        else
-            element.className='box';
-    }
-    //console.log(flag);
-    window.flag=true;
-}
-
 function openModal(flag) {
     if (flag) {
         $("#tab-1").prop('checked', true);
     } else {
         $("#tab-2").prop('checked', true);
     }
-}
-
-function buyTicket(flag) {
-    //console.log(flag);
-    window.flag=false;
 }
